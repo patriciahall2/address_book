eec0023553cf8377d9cced4a49f5dd52987e72c6
evan stewart and Sarah Read Brown <stewarea@gmail.com>
2016-03-14 20:36:23 -0700

    reworked the clear fields into the business dev part of jquery

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,16 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input#new-street").val("");
+  $("input#new-city").val("");
+  $("input#new-state").val("");
+}
 $(document).ready(function()  {
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
@@ -60,15 +70,9 @@ $(document).ready(function()  {
 
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
-
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-street").val("");
-    $("input#new-city").val("");
-    $("input#new-state").val("");
+    resetFields();
   });
 });
